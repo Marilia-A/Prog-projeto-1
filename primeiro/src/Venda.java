666767fd1b1645d75c2d96be328c23dd5920ec9f
Marilia-A <mariliaalmeidarosa01@gmail.com>
2025-09-17 21:21:21 -0300

    Att2

--- primeiro/src/Venda.java
+++ primeiro/src/Venda.java
@@ -1,17 +1,51 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 public class Venda {
-    public static void main(String[] args) throws Exception {
-      Produto prod = new Produto();
-        prod.setNome("Fita Isolante");
-        prod.setMarca("3M");
-        prod.setPreco(3.50);
-        prod.setQtde(10);
-     
-        System.out.println("Nome: " + prod.getNome());
-        System.out.println("Marca: " + prod.getMarca());
-        System.out.println("Preço: " + prod.getPreco());
-        System.out.println("Quantidade: " + prod.getQtde());
-        System.out.println("Total: " + (prod.getPreco() * prod.getQtde()));  
+
+    private LocalDate data;
+    private String numeroVenda;
+    private Produto produto;
+    private Integer qtdeVendida;
+
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+    public LocalDate getData() {
+        return data;
+    }
+    public String getNumeroVenda() {
+        return numeroVenda;
+    }
+    public void setNumeroVenda(String numeroVenda) {
+        this.numeroVenda = numeroVenda;
+    }
+    public Produto getProduto() {
+        return produto;
+    }
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+    public Integer getQtdeVendida() {
+        return qtdeVendida;
+    }
+    public void setQtdeVendida(Integer qtdeVendida) {
+        this.qtdeVendida = qtdeVendida;
+    }
+
+    public void ImprimirVenda(){
+        System.out.println("------ DADOS DE VENDA ------");
+        System.out.println("Número da venda: " +getNumeroVenda());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dataFormatada = getData().format(formatter);
+        System.out.println("Data da venda: " + dataFormatada);
+
+        System.out.println("------ DADOS DO PRODUTO ------");
+        System.out.println("Produto Vendido: " +getProduto().getNome());
+        System.out.println("Marca: " +getProduto().getMarca());
+        System.out.println("Preço: " +getProduto().getPreco());
+        System.out.println("Quantidade em estoque: " +getProduto().getQtde());
+        System.out.println("-----------------------------");
     }
 }
-// Venda é uma classe que representa o processo de venda de um produto
-// Atributos são as características do produto que está sendo vendido
